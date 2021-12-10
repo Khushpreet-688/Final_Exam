e081465cd4924f2d434174728e32b675f1cdcbb7
MSI <ksidhukhushpreet@gmail.com>
2021-12-10 13:09:02 -0800

    modify script.js, add event listener to submit button that when clicked calls the submit function. This function stores all the values of input fields of the form in an object and logs the object to the console

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,24 @@
+
 function change_mode(){
     document.body.classList.toggle('dark-theme')
 }
 
-
 toggle = document.querySelector(".toggle")
 toggle.addEventListener('change', change_mode)
+
+function submit(e){
+    frm_name = document.querySelector("#name")
+    frm_email = document.querySelector("#email")
+    frm_msg = document.querySelector("#msg")
+    let form_info = {
+        name: `${frm_name.value}`,
+        email: `${frm_email.value}`,
+        message: `${frm_msg.value}`
+    }
+
+    console.log(form_info)
+    e.preventDefault()
+}
+
+submit_btn = document.querySelector("#frm-btn")
+submit_btn.addEventListener('click', submit)
